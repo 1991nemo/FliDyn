5f7bbd5716742f3e1cc3e0baafe311a194fcfb43
1991nemo <46131508+1991nemo@users.noreply.github.com>
2021-07-23 14:57:29 +0430

    Add files via upload

---
+++ FliDynModule/ODE/OdeCommon.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace FliDynModule.ODE
+{
+    public class Options
+    {
+        public double InitialStep { get; set; }
+        public double AbsoluteTolerance { get; set; }
+        public double RelativeTolerance { get; set; }
+        public double OutputStep { get; set; }
+        public double MaxStep { get; set; }
+        public double MinStep { get; set; }
+        public double MaxScale { get; set; }
+        public double MinScale { get; set; }
+        public int NumberOfIterations { get; set; }
+        public Matrix Jacobian { get; set; }
+        public SparseMatrix SparseJacobian { get; set; }
+        public Options()
+        {
+            InitialStep = 0.0d;
+            AbsoluteTolerance = 1e-6;
+            RelativeTolerance = 1e-3;
+            MaxStep = Double.MaxValue;
+            MinStep = 0.0d;
+            MaxScale = 1.1d;
+            MinScale = 0.9d;
+            OutputStep = 0.0d;
+            NumberOfIterations = 5;
+        }
+
+        private static readonly Options defaultOpts = new Options();
+        public static Options Default
+        {
+            get { return defaultOpts; }
+        }
+    }
+    public struct SolPoint
+    {
+        private Vector x; //Problem's phase variables
+        private double t; //Current time
+        public Vector X
+        {
+            get { return x; }
+        }
+        
+        public double T
+        {
+            get { return t; }
+        }
+        internal SolPoint(double t, Vector x)
+        {
+            this.x = x;
+            this.t = t;
+        }
+    }
+}
---
+++ FliDynModule/ODE/OdeHelpers.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using FliDynModule;
+
+namespace FliDynModule.ODE
+{
+    public static class OdeHelpers
+    {
+        public static IEnumerable<SolPoint> SolveTo(this IEnumerable<SolPoint> solution, double to)
+        {
+            return solution.TakeWhile(p => p.T <= to);
+        }
+        public static IEnumerable<SolPoint> SolveFromTo(this IEnumerable<SolPoint> solution, double from, double to)
+        {
+            return solution.SkipWhile(p => p.T < from).TakeWhile(p => p.T <= to);
+        }
+        public static IEnumerable<SolPoint> SolveFromToStep(this IEnumerable<SolPoint> solution, double from, double to, double step)
+        {
+            return solution.WithStep(step).SkipWhile(p => p.T < from).TakeWhile(p => p.T <= to);
+        }
+        public static IEnumerable<SolPoint> WithStep(this IEnumerable<SolPoint> solution, double delta)
+        {
+            var en = solution.GetEnumerator();
+            if (!en.MoveNext())
+                yield break;
+            SolPoint prev = en.Current;
+            double n = Math.Ceiling(prev.T / delta);
+            double tout = n * delta;
+            if (tout == prev.T)
+                yield return prev;
+            n++;
+            tout = n * delta;
+            while (true)
+            {
+                if (!en.MoveNext())
+                    yield break;
+                SolPoint current = en.Current;
+                while (current.T >= tout)
+                {
+                    yield return new SolPoint(tout, Vector.Lerp(tout, prev.T, prev.X, current.T, current.X));
+                    n++;
+                    tout = n * delta;
+                }
+                prev = current;
+            }
+        }
+        public static IEnumerable<SolPoint> AppendStep(this IEnumerable<SolPoint> solution)
+        {
+            var en = solution.GetEnumerator();
+            if (!en.MoveNext())
+                yield break;
+            SolPoint prev = en.Current;
+            double[] temp = new double[prev.X.Length + 1];
+            Array.Copy(prev.X, temp, prev.X.Length);
+            yield return new SolPoint(prev.T, new Vector((double[])temp.Clone()));
+            while (en.MoveNext())
+            {
+                SolPoint curr = en.Current;
+                Array.Copy(curr.X, temp, curr.X.Length);
+                temp[curr.X.Length] = curr.T - prev.T;
+                yield return new SolPoint(curr.T, new Vector((double[])temp.Clone()));
+                prev = curr;
+            }
+            yield break;
+        }
+    }
+}
